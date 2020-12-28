28a5d961150ea451c7893a339cc4393d3cc5c2ec
suren <suren-m>
2020-12-28 14:43:57 +0000

    added some initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
-use residue::{self, add};
+use residue::{self, game::Player};
 
 fn main() {
-    let a = add();
-    println!("Hello, world!");
+    println!("\n...starting...\n");
+    let p = Player::new();
+
+    let mut board: [[char; 3]; 4] = [['.'; 3]; 4];
+
+    board[1][2] = p.get_avatar();
+
+    'outer: for i in board.iter() {
+        'inner: for j in i.iter() {
+            print!("{}\t", j.to_owned());
+        }
+        println!("\n");
+    }
+    let position = p.get_position();
+
+    println!("{:?}", p);
 }
