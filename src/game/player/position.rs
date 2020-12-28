6864c4133f1def550662837e6d859163ed533bf9
suren <suren-m>
2020-12-28 20:14:32 +0000

    added unit test for position add

--- src/game/player/position.rs
+++ src/game/player/position.rs
@@ -1,6 +1,6 @@
 use std::ops::Add;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Position {
     pub x: u8,
     pub y: u8,
@@ -14,5 +14,22 @@ impl Add for Position {
             x: self.x + other.x,
             y: self.y + other.y,
         }
+    }
+}
+
+#[cfg(test)]
+mod player_tests {
+
+    use super::*;
+
+    #[test]
+    fn addition_of_two_positions(){
+        let p1 = Position{ x: 1, y: 1};
+        let p2 = Position{ x: 2, y: 2};
+        
+        let res = p1 + p2;
+        let exp = Position { x: 3, y: 3};
+        
+        assert_eq!(res, exp);
     }
 }
